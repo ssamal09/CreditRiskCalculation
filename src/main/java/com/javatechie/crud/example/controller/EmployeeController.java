46fbd365a9975300b88c3638a7048d3bb3cc49e0
ssamal09 <shaktisamal90@gmail.com>
2021-11-13 21:25:00 +0530

    SRSD-134-Product and Employee added.

---
+++ src/main/java/com/javatechie/crud/example/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.javatechie.crud.example.controller;
+
+import com.javatechie.crud.example.entity.Employee;
+import com.javatechie.crud.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+
+    @PostMapping("/addEmployee")
+    public Employee saveEmployee(@RequestBody Employee employee)
+    {
+        return employeeService.saveEmployee(employee);
+    }
+}
